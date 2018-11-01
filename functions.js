8e048d2ebab98898eff1f632696d66c081f91206
Dej <dejanualex@yahoo.com>
2018-11-01 16:29:50 +0200

    first commit

---
+++ functions.js
@@ -0,0 +1,12 @@
+function greet (name,surname) {
+   console.log("hello"+name+surname); 
+}
+
+greet('John',"bovi");
+
+function square (number){
+    return number * number;
+}
+
+let x = square(2);
+console.log(x);
---
+++ index.js
@@ -0,0 +1,28 @@
+
+//Dynamic Typing - type is determined at runtime
+console.log(typeof name);
+name = 1;
+console.log(typeof name);
+
+//declare a variable old way was var
+let name = "test"; //string literal
+//declare a constant
+const interestRate = 0.3;
+
+//Primitive aka Value Types: String,Number,Boolean,undefined and null
+
+//Reference Types: Object, Array, Function
+
+//Object
+let person = {
+    name: "msh",
+    age: 30
+};
+
+
+
+//Array
+let colors = ['red', 'blue'];
+colors = 123
+console.log(colors)
+
